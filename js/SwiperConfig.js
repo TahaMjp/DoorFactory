d0a59a9524e82b334acfdc19b05f614799673102
TahaMajlesi <186780445+TahaMjp@users.noreply.github.com>
2025-04-08 09:14:36 -0700

    Major update

--- js/SwiperConfig.js
+++ js/SwiperConfig.js
@@ -4,3 +4,19 @@ let BannerSwiper = new Swiper(".BannerSwiper", {
   },
   loop: true,
 });
+
+//  Initialize Swiper
+let AboutBanner = new Swiper(".AboutBanner", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//  Initialize Swiper
+let modulsSwiper = new Swiper(".modulsSwiper", {
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+});
